4289c76d1208c0898dde0931e15c7c5b7e760233
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-10-30 18:47:22 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
 <?php
-
 //Check for any errors (uneeded for now)
 /*
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
 */
-
 //Grab required files 
 session_start();
 //THIS FILE MAY NOT BE NEEDED AFTER DELETE IF NOT IN USE
@@ -13,11 +11,8 @@ require_once('logger.inc');
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-
 //Add new connection for logging to rabbitMQ
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-
-
 if (isset($argv[1]))
 {
   $msg = $argv[1];
@@ -32,17 +27,13 @@ $request['type'] = "login";
 $request['username'] = $_POST['username'];
 $request['password'] = $_POST['pass'];
 $request['message'] = $msg;
-
 echo "REQUEST ERROR!" . PHP_EOL;
 logger( __FILE__ . " : " . __LINE__ . " :error: " . "CHECK CONNECTION");
-
 $response = $client->send_request($request);
 echo "Request Failure".PHP_EOL;
 print_r($response);
-
 //See if there is any response msg as of now we don't have.
 echo "\n\n";
-
 //If not in the database
 if ($response == 0) {
 	//$date = date_create();
@@ -50,11 +41,10 @@ if ($response == 0) {
 	echo "REQUEST ERROR!" . PHP_EOL;
 	logger( __FILE__ . " : " . __LINE__ . " :error: " . "Bad Request Type, check connection with database");
 	}
-
-
 else {
-	$_SESSION["logged"] = true;
+	$_SESSION['logged'] = true;
 	$_SESSION["username"] = $request['username'];
+	//$name = $_SESSION['username'];
 	header("Location: loginsuccess.php");
 }
 ?>
