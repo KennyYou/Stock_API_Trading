bf410889fafa7f98e273abf6bd038e0f17ca8a05
HemanthSesha <55961217+HemanthSesha@users.noreply.github.com>
2019-10-31 21:37:49 -0400

    Update sell.php

--- sell.php
+++ sell.php
@@ -4,7 +4,7 @@
 
 <div class="topnav">
 
-<a class="active" href="#HOME">Home</a> 
+<a class="active" href="loginsuccess.php">Home</a> 
 <a class="active" href="#profile">Portfolio</a> 
 <a class="active" href="buy.php">BUY</a> 
 <a class="active" href="index.html">Logout<a>
