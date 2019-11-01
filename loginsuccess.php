b4b3c79191feaf54c7d96ae28ab084bc4f74a019
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-11-01 00:43:25 -0400

    Update loginsuccess.php
    Midterm Deliver.

--- loginsuccess.php
+++ loginsuccess.php
@@ -8,6 +8,7 @@
  session_start(); 
 if ($_SESSION['logged'] == true) 
 {
+   echo "Welcome: ";
    echo $_SESSION["username"];
 //echo "USER NAME DISPLAYED";
 //$user = $_SESSION["user"];
@@ -21,8 +22,7 @@ if ($_SESSION['logged'] == true)
 <div class="topnav">
 
 <a class="active" href="loginsuccess.php">HOME</a> 
-<a class="active" href="#profile">PROFILE</a> 
-<a class="active" href="#Suggestions">SUGGESTIONS</a> 
+<a class="active" href="profile.php">PROFILE</a> 
 <a class="active" href="buy.php">BUY</a> 
 <a class="active" href="sell.php">SELL</a> 
 <a class="active" href="index.html">Logout<a>
@@ -32,12 +32,6 @@ if ($_SESSION['logged'] == true)
 <h2>Search a Stock:</h2>
 	<input type="text" name="search1S" placeholder="Enter a stock symbol:" required>
 	<button type="submit">Search</button><br>
-    <thead>
-<tr>
-<th> Stock Info </th>
-</tr>
-</thead>
-<tbody>
 
 </form>
 <table class ="table table-bordered">
@@ -76,20 +70,16 @@ else
 {
   $msg = "search1S";
 }
-
 //Send search request over
 $request['type'] = "search1S";
 $request['search1S'] = $_POST['search1S'];
 $request['message'] = $msg;
-
-
 $response = $client->send_request($request);
 //PHP_EOL should echo in from backend
 echo "".PHP_EOL;
 print_r($response);
 echo"\n";
 }
-
 //Request Balance automatically
 require_once('path.inc');
 require_once('get_host_info.inc');
@@ -103,19 +93,15 @@ else
 {
   $msg = "requestBalance";
 }
-
 //Send search request over
 $request['type'] = "requestBalance";
 $request['username'] = $_SESSION["username"];
 $request['message'] = $msg;
-
-
 $response = $client->send_request($request);
 //PHP_EOL should echo in from backend
 echo "".PHP_EOL;
 print_r($response['bal']);
 echo"\n";
-
 ?>
 
 <?php
@@ -136,45 +122,19 @@ else
 {
   $msg = "search1N";
 }
-
 //Send search request over
 $request['type'] = "search1N";
 $request['search1N'] = $_POST['search1N'];
 $request['message'] = $msg;
-
-
 $response = $client->send_request($request);
 //PHP_EOL should echo in from backend
-echo "".PHP_EOL;
+
+echo "Balance is: ".PHP_EOL;
+echo "Balance is: ";
 print_r($response);
 echo"\n";
 }
 
-//Request Balance automatically
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
-{
-  $msg = $argv[1];
-}
-else
-{
-  $msg = "requestBalance";
-}
-
-//Send search request over
-$request['type'] = "requestBalance";
-$request['username'] = $_SESSION["username"];
-$request['message'] = $msg;
-
-
-$response = $client->send_request($request);
-//PHP_EOL should echo in from backend
-echo "".PHP_EOL;
-print_r($response['bal']);
-echo"\n";
 
 ?>
 
