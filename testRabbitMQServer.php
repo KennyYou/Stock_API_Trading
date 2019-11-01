b08942276a53e573e3a9b2e009f9b58e7f3cd0c7
mpmNJIT <31703101+mpmNJIT@users.noreply.github.com>
2019-11-01 00:45:12 -0400

    A whole lot of stock stuff
    Implemented buying and selling of stocks, as well as recording the buying and selling. Separated search into a "simple" one that searches symbols and "detailed" that returns the open, high, low, close, and volume of the closest matching stock.

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
@@ -75,6 +75,41 @@ foreach($jsonarray['bestMatches'] as $variable) {
 return $output;
 }
 
+function doDetailSearch($search) {
+	$ch = curl_init("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" . $search . "&apikey=9J4N8FA67HVHYZG0");
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$curl_results = curl_exec($ch);
+	curl_close($ch);
+
+$jsonarray = json_decode($curl_results, true); 
+	
+$symbol = $jsonarray['bestMatches'][0]['1. symbol'];
+$name = $jsonarray['bestMatches'][0]['2. name'];
+//Curl for each symbol and get open, high, low, and close.
+$ch2 = curl_init("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=".$symbol."&interval=1min&apikey=9J4N8FA67HVHYZG0");
+curl_setopt($ch2, CURLOPT_HEADER, 0);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, TRUE);
+$curl_results2 = curl_exec($ch2);
+curl_close($ch2);
+$jsonarray2 = json_decode($curl_results2, true);
+$time = $jsonarray2['Meta Data']['3. Last Refreshed'];
+$open = $jsonarray2['Time Series (1min)'][$time]['1. open'];
+$high = $jsonarray2['Time Series (1min)'][$time]['2. high'];
+$low = $jsonarray2['Time Series (1min)'][$time]['3. low'];
+$close = $jsonarray2['Time Series (1min)'][$time]['4. close'];
+$volume = $jsonarray2['Time Series (1min)'][$time]['5. volume'];
+$output .= "<td>".$symbol." </td>";
+$output .= "<td>".$name." </td>";
+$output .= "<td>".$open." </td>";
+$output .= "<td>".$high." </td>";
+$output .= "<td>".$low." </td>";
+$output .= "<td>".$close." </td>";
+$output .= "<td>".$volume." </td>";
+$output .= "</tr>";
+return $output;
+}
+
 function doRequestBalance($username) {
 	global $db;
 	//save query as variable
@@ -86,12 +121,18 @@ function doRequestBalance($username) {
 	errorCheck($db);
 }
 
+function doShowOwnedStock($username) {
+	global $db;
+	$ShowOwnedStock = mysqli_query($db, "SELECT symbol FROM ".$username."_stocks");
+	}
+
 function doBuyStock($username, $symbol, $amount) {
 	global $db;
 	//Fetch current stock info from API
+	$alpha_vantage = new Client('9J4N8FA67HVHYZG0');
 	$stockinfo = $alpha_vantage
-    		->stock()
-    		->intraday($symbol, AlphaVantage\Resources\Stock::INTERVAL_1MIN);
+    	->stock()
+    	->intraday($symbol, AlphaVantage\Resources\Stock::INTERVAL_1MIN);
 	$fetchedsymbol = $stockinfo["Meta Data"]["2. Symbol"];
 	//Check if symbol exists
 	if ($symbol != $fetchedsymbol)
@@ -112,6 +153,7 @@ function doBuyStock($username, $symbol, $amount) {
 		$bal= $r["bal"];
 		if ($bal - $total < 0)
 		{
+			echo "Nope! Not enough cash.\n\n";
 			return "ERROR! Insufficent funds for stock purchase!";
 		}
 	};
@@ -119,16 +161,69 @@ function doBuyStock($username, $symbol, $amount) {
 	mysqli_query ($db, $s);
 	$s = "insert into trading (username, type, symbol, shares, date, cost) values ('$username', 'buying', '$symbol', '$i_amount', NOW(), $total)";
 	mysqli_query ($db, $s);
-	$s = "insert into ".$username."_stocks (symbol, amt) values ('$symbol',)";
+	//If new stock, insert into table. Else, update existing data.
+	$s = "select * from ".$username."_stocks where symbol = '$symbol'";
+	$testq = mysqli_query($db, $s);
+	$testarray = mysqli_fetch_array($testq);
+	if (is_null($testarray))
+	{
+		$s = "insert into ".$username."_stocks (symbol, amt) values ('$symbol','$i_amount')";
 	mysqli_query ($db, $s);
-
+	}
+	else
+	{
+	$s = "update ".$username."_stocks set amt = amt + '$i_amount' where symbol = '$symbol' ";
+	mysqli_query ($db, $s);
+	}
+	echo "Successful buy!\n\n";
 	return "Your transaction was successful! You have bought ".$i_amount." shares of ".$symbol." stock for $".$total."!";
+	errorCheck($db);
+}
+
+function doSellStock($username, $symbol, $amount) {
+	global $db;
+	//Fetch stock amounts from [username]_stocks
+	$s = "select * from ".$username."_stocks where symbol = '$symbol'";
+	$testq = mysqli_query($db, $s);
+	$testarray = mysqli_fetch_array($testq);
+	if (is_null($testarray))
+	{
+		echo "User doesn't have that stock!\n\n";
+		return "ERROR! You haven't bought that stock!";
+	}
+	if ($testarray['amt'] - (intval($amount)) < 0)
+	{
+		echo "User doesn't have enough stock!\n\n";
+		return "ERROR! You don't have enough of that stock!";
+	}
+	//Fetch current stock info from API
+	$alpha_vantage = new Client('9J4N8FA67HVHYZG0');
+	$stockinfo = $alpha_vantage
+    	->stock()
+    	->intraday($symbol, AlphaVantage\Resources\Stock::INTERVAL_1MIN);
+	//Get stock HIGH as sell price (NBBO)
+	$stocktime = $stockinfo["Meta Data"]["3. Last Refreshed"];
+	$stockpricestring = $stockinfo["Time Series (1min)"][$stocktime]["2. high"];
+	//convert price and amount to float, multiply stock by amount, then round.
+	$stockprice = floatval($stockpricestring);
+	$i_amount = intval($amount);
+	$t = $stockprice * $i_amount;
+	$total = round($t,2);
+	$s = "update students set bal = bal + '$total' where BINARY username = '$username' ";
+	mysqli_query ($db, $s);
+	$s = "insert into trading (username, type, symbol, shares, date, cost) values ('$username', 'selling', '$symbol', '$i_amount', NOW(), $total)";
+	mysqli_query ($db, $s);
+	$s = "update ".$username."_stocks set amt = amt - '$i_amount' where symbol = '$symbol' ";
+	mysqli_query ($db, $s);
+	echo "Successful sell!\n\n";
+	return "Your transaction was successful! You have sold ".$i_amount." shares of ".$symbol." stock for $".$total."!";
 	errorCheck($db);
 }
 	
 
 //CODE "STARTS" HERE (server recieves requests then chooses function)
 function requestProcessor($request) {
+	$alpha_vantage = new Client('9J4N8FA67HVHYZG0');
 	echo "Received a request".PHP_EOL;
 	var_dump($request);
 	if(!isset($request['type'])) {
@@ -145,14 +240,23 @@ function requestProcessor($request) {
 	case "validate_session":
 		return doValidate($request['sessionId']);
 
-	case "search":
-		return doSearch($request['search']);
+	case "search1S":
+		return doSearch($request['search1S']);
+
+	case "search1N":
+		return doDetailSearch($request['search1N']);
 
 	case "requestBalance":
 		return doRequestBalance($request['username']);
 
+	case "showOwnedStock":
+		return doShowOwnedStock($request['username']);
+
 	case "buyStock":
 		return doBuyStock($request['username'], $request['symbol'], $request['amount']);
+	
+	case "sellStock":
+		return doSellStock($request['username'], $request['symbol'], $request['amount']);
 	}
 	return array("returnCode" => '0', 'message'=>"Server received request and processed.");
 }
