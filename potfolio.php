b396f85354f7d01f08dbad7d8fdcc6f93bda70e7
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-11-13 17:16:43 -0500

    Update potfolio.php
    Changed to show transaction history

--- potfolio.php
+++ potfolio.php
@@ -20,11 +20,11 @@ if ($_SESSION['logged'] == true)
    echo "Welcome to your profile: ";
    echo $_SESSION["username"];
 }
-
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+
 if (isset($argv[1]))
 {
   $msg = $argv[1];
@@ -33,7 +33,6 @@ else
 {
   $msg = "showOwnedStock";
 }
-
 //Send search request over
 $request = array();
 $request['type'] = "showOwnedStock";
@@ -43,45 +42,20 @@ echo"Your stocks are as follows:";
 echo "<br>";
 //Send msg
 $request['message'] = $msg;
-
 $response = $client->send_request($request);
 //PHP_EOL should echo in from backend
 echo "".PHP_EOL;
+//Only display up to 14 stocks saved
+for ($i = 0; $i <= 14 + 1; $i++) {
+    print_r($response[$i]['symbol']);
+	print_r(" ");
+    print_r($response[$i]['amt']);
+	echo "<br>";
+}
 
-print_r($response[0]["symbol"]);
-print_r(" ");
-print_r($response[0]["amt"]);
-echo "<br>";
-
-print_r($response[1]["symbol"]);
-print_r(" ");
-print_r($response[1]["amt"]);
-echo "<br>";
-
-
-print_r($response[2]["symbol"]);
-print_r(" ");
-print_r($response[2]["amt"]);
-echo "<br>";
-
-print_r($response[3]["symbol"]);
-print_r(" ");
-print_r($response[3]["amt"]);
-echo "<br>";
-
-print_r($response[4]["symbol"]);
-print_r(" ");
-print_r($response[4]["amt"]);
-echo "<br>";
-
-
-print_r($response[5]["symbol"]);
-print_r(" ");
-print_r($response[5]["amt"]);
-echo "<br>";
-echo"\n";
 
 //Printing out transaction history 
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 if (isset($argv[1]))
 {
   $msg1 = $argv[1];
@@ -90,7 +64,6 @@ else
 {
   $msg1 = "showTrading";
 }
-
 $request1 = array();
 $request1['type'] = "showTrading";
 $request1['username'] = $_SESSION["username"];
@@ -99,22 +72,23 @@ echo"Your transaction history are as follows:";
 echo "<br>";
 //Send msg
 $request1['message'] = $msg1;
-
 $response1 = $client->send_request($request1);
 //PHP_EOL should echo in from backend displaying transaction history 
 echo "".PHP_EOL;
-for ($i = 0; $i < count($request1); $i++) {
-    print_r($response[$i]['type']);
+for ($i = 0; $i <= 10; $i++) {
+    print_r($response1[$i]['type']);
 	print_r(" ");
-    print_r($response[$i]['symbol']);
+    print_r($response1[$i]['symbol']);
 	print_r(" ");
-    print_r($response[$i]['shares']);
+    print_r($response1[$i]['shares']);
 	print_r(" ");
-    print_r($response[$i]['date']);
+    print_r($response1[$i]['date']);
 	print_r(" ");
-    print_r($response[$i]['cost']);
+    print_r($response1[$i]['cost']);
 	echo "<br>";
+
 }
+
 ?>
 
 
