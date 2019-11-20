411ac29fc26b21550ff89a7aca49a8057a0faa56
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-11-20 17:11:35 -0500

    Update login.php
    updated persis logging

--- login.php
+++ login.php
@@ -1,11 +1,9 @@
 <?php
-
 //Check for any errors (uneeded for now)
 /*
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
 */
-
 //Grab required files 
 session_start();
 //THIS FILE MAY NOT BE NEEDED AFTER DELETE IF NOT IN USE
@@ -16,8 +14,6 @@ require_once('rabbitMQLib.inc');
 
 //Add new connection for logging to rabbitMQ
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-
-
 if (isset($argv[1]))
 {
   $msg = $argv[1];
@@ -33,34 +29,23 @@ $request['username'] = $_POST['username'];
 $request['password'] = $_POST['pass'];
 $request['message'] = $msg;
 
-for ($x = 0; $x <= 3; $x++) {
-echo "REQUEST ERROR!" . PHP_EOL;
-logger( __FILE__ . " : " . __LINE__ . " :error: " . "CHECK CONNECTION");
-}
 
 $response = $client->send_request($request);
 echo "Request Failure".PHP_EOL;
 print_r($response);
-
 //See if there is any response msg as of now we don't have.
 echo "\n\n";
-
 //If not in the database
 if ($response == 0) {
 	//$date = date_create();
 	header("location:loginerror.html");
-	//Sends error log out 3 times
-	for ($x = 0; $x <= 3; $x++) {
-    echo "REQUEST ERROR!" . PHP_EOL;
+	echo "REQUEST ERROR!" . PHP_EOL;
 	logger( __FILE__ . " : " . __LINE__ . " :error: " . "Bad Request Type, check connection with database");
-	} 
-	
 	}
-
-
 else {
-	$name = $_SESSION['username'];
-	//$date = date_create();
+	$_SESSION['logged'] = true;
+	$_SESSION["username"] = $request['username'];
+	//$name = $_SESSION['username'];
 	header("Location: loginsuccess.php");
 }
 ?>
