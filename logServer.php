f6a0f69a76948205b776ac8b97094b9f56dbc388
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-11-20 16:09:20 -0500

    Update logServer.php
    updated comments

--- logServer.php
+++ logServer.php
@@ -4,10 +4,12 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function logProcessor($log)
+function logging($log)
 {
  	echo "Logging Error: ".PHP_EOL; 
 	echo $log;
+	//put file into location @ logs.txt
+	//users have to make this file
 	file_put_contents("/home/tmp/logs.txt",$log.PHP_EOL,FILE_USE_INCLUDE_PATH | FILE_APPEND);
 
 }
@@ -15,7 +17,7 @@ function logProcessor($log)
 $server = new rabbitMQServer("rabbitMQErrorLog.ini","testServer");
 echo var_dump($server);
 
-$server->process_requests('logProcessor');
+$server->process_requests('logging');
 exit();
 ?>
 
