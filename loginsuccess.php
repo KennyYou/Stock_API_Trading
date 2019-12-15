a6a55b24840f14c5ead77cb41a6bc558ea25df4d
Kenny Y <39010375+KennyYou@users.noreply.github.com>
2019-12-15 13:32:30 -0500

    RSS implemented

--- loginsuccess.php
+++ loginsuccess.php
@@ -10,15 +10,68 @@ if ($_SESSION['logged'] == true)
 {
    echo "Welcome: ";
    echo $_SESSION["username"];
+   echo "News feed: (Powered by RSS)";   
 //echo "USER NAME DISPLAYED";
 //$user = $_SESSION["user"];
 // echo $user;
 }
 //echo "USER NOT DISPLAYED";
  ?>
+ 
+ <?php
+//Doing RSS from google news source
+
+$xml=("https://news.google.com/rss/search?q=stocks&hl=en-US&gl=US&ceid=US:en");
+
+$xmlDoc = new DOMDocument();
+$xmlDoc->load($xml);
+
+//get elements from "<channel>"
+$channel=$xmlDoc->getElementsByTagName('channel')->item(0);
+$channel_title = $channel->getElementsByTagName('title')
+->item(0)->childNodes->item(0)->nodeValue;
+$channel_link = $channel->getElementsByTagName('link')
+->item(0)->childNodes->item(0)->nodeValue;
+$channel_desc = $channel->getElementsByTagName('description')
+->item(0)->childNodes->item(0)->nodeValue;
+
+//output elements from "<channel>"
+$ee = ("<p><a href='" . $channel_link
+  . "'>" . $channel_title . "</a>");
+echo("<br>");
+echo($channel_desc . "</p>");
+
+//Output the 
+$x=$xmlDoc->getElementsByTagName('item');
+for ($i=0; $i<=10; $i++) {
+  $item_title=$x->item($i)->getElementsByTagName('title')
+  ->item(0)->childNodes->item(0)->nodeValue;
+  $item_link=$x->item($i)->getElementsByTagName('link')
+  ->item(0)->childNodes->item(0)->nodeValue;
+  $item_desc=$x->item($i)->getElementsByTagName('description')
+  ->item(0)->childNodes->item(0)->nodeValue;
+  echo ("<p><a href='" . $item_link
+  . "'>" . $item_title . "</a>");
+  echo ("<br>");
+  //echo ($item_desc . "</p>");
+}
+//END RSS FEATURE
+?>
+
+<?php
+// the message should run on script startup
+$msg = "TESTING\nFirst line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("ky77@njit.edu","Testing Email",$msg);
+?>
+
 </head>
 <body>
-
+<h2> ___________________________ </h2>
 <div class="topnav">
 
 <a class="active" href="loginsuccess.php">HOME</a> 
@@ -26,7 +79,7 @@ if ($_SESSION['logged'] == true)
 <a class="active" href="buy.php">BUY</a> 
 <a class="active" href="sell.php">SELL</a> 
 <a class="active" href="graph.html">GRAPH</a> 
-<a class="active" href="index.html">Logout<a>
+<a class="active" href="index.html">LOGOUT<a>
 </div>
 
 <form method="POST">
@@ -124,7 +177,6 @@ else
 {
   $msg = "search1N";
 }
-
 //Send search request over
 $request['type'] = "search1N";
 $request['search1N'] = $_POST['search1N'];
@@ -135,8 +187,6 @@ echo "".PHP_EOL;
 print_r($response);
 echo"\n";
 }
-
-
 ?>
 
 
