2a46315f865d3420d7480850ff935bedbca8d6c5
Joseph Mauro <49416852+too-mauro@users.noreply.github.com>
2019-12-16 18:38:58 -0500

    Update deploymentServer.php

--- deployment/deploymentServer.php
+++ deployment/deploymentServer.php
@@ -1,5 +1,4 @@
 #!/usr/bin/php
-
 <?php
 require_once("../path.inc");
 require_once("../rabbitMQLib.inc");
@@ -7,41 +6,45 @@ require_once("../get_host_info.inc");
 include("deployDB.php");
 
 // FUNCTION LIST
-function doDeployPkg($version, $user, $ip, $namePkg, $description, $pkgPath) {
+function doDeployPkg($version, $user, $ip, $pkgName, $description, $pkgPath, $tierType) {
 	
 	global $db;
 	// send package over Secure Copy (SCP) using target machine's user password
-	$output = shell_exec("sshpass -f '/home/jdm68/pwDir/jdm68' | sudo tar czf /home/jdm68/packages/$pkgName-$version.tar.gz $pkgPath;
-		sshpass -f '/home/jdm68/pwDir/$user' | scp -P 22 /home/jdm68/packages/$namePkg-$version.tar.gz $user@$ip:~/$namePkg-$version.tar.gz");
+	$output = shell_exec("sshpass -f '/home/jdm68/pwDir/$user' | scp -P 22 /home/jdm68/packages/$tierType-$pkgName-$version.tar.gz $user@$ip:~/$tierType-$pkgName-$version.tar.gz");
 	
 	// insert package information in database deployment table
 	$connect = mysqli_connect('localhost', 'deploy', '490password', 'deployment');
 	if (!$connect) {echo ("MySQL ERROR: " . mysqli_connect_error());}
 	else {echo "SUCCESS: connection established to DB\n";}
-
-	if ($query = mysqli_prepare($connect, "INSERT INTO packages (pkgName, description, version, validated, pkgPath, uploadDateTime) VALUES (?,?,?,?,?,?)")) {
-		$query ->bind_param('ssssss', $namePkg, $description, $version, $validated, $pkgPath, NOW());
+	
+	$validated = 0;
+	// $tierType = 0;
+	
+	if ($query = mysqli_prepare($connect, "INSERT INTO packages (pkgName, description, version, validated, pkgPath, uploadDateTime, tierType) VALUES (?,?,?,?,?,?,?)")) {
+		$dt = date('Y-m-d H:i:s');
+		$query ->bind_param('sssssss', $pkgName, $description, $version, $validated, $pkgPath, $dt, $tierType);
 		$query ->execute();
 		if ($query->affected_rows < 0) {
 			echo "ERROR: " . $query->error;
 			$query->close();
 		}
 		else {echo "Data successfully inserted into deployment database! Rows changed: " . $query -> affected_rows . PHP_EOL;}
-		
+
 	errorCheck($db);
 	}
 }
 
 function doGetPkg($request) {
-
+	
 	global $db;
 	echo "REQUEST TAKEN".PHP_EOL;
 	var_dump($request);
 	if(!isset($request['type'])) {return "ERROR: Message type not set!";}
 	$connect = mysqli_connect("localhost", "deploy", "490password", "deployment");
-
+	
 	if(!$connect) {die("ERROR: No connection." . mysqli_connect_error());}
 	else {echo "WE IN\n";}
+
 	var_dump($server);
 	errorCheck($db);
 }
@@ -53,9 +56,9 @@ function doRollback($pkgName, $version) {
 	$connect = mysqli_connect('localhost', 'deploy', '490password', 'deployment');
 	if (!$connect) {echo ("mySQL ERROR: " . mysqli_connect_error());}
 	else {echo "SUCCESS: Connection Established to DB\n";}
-
+	
 	// mark passed package version as bad
-	if ($query = mysqli_prepare($connect, "UPDATE packages SET validated = 'F' WHERE pkgName = $pkgName AND version = $version")) {
+	if ($query = mysqli_prepare($connect, "UPDATE packages SET validated = '0' WHERE pkgName = $pkgName AND version = $version")) {
 		$query ->execute();
 		if ($query->affected_rows < 0) {
 			echo "ERROR: " . $query->error;
@@ -65,7 +68,7 @@ function doRollback($pkgName, $version) {
 	}
 
 	// get most recent validated package and push it
-	if ($query = mysqli_prepare($connect, "SELECT version FROM packages WHERE pkgName = $namePkg AND validated = 'T' ORDER BY DATE DESC")) {
+	if ($query = mysqli_prepare($connect, "SELECT version FROM packages WHERE pkgName = $namePkg AND validated = '1' ORDER BY DATE DESC")) {
 		$query ->execute();
 		echo "" .PHP_EOL;
 	}
@@ -78,16 +81,16 @@ function doValidatePkg($pkgName, $version) {
 	$connect = mysqli_connect('localhost', 'deploy', '490password', 'deployment');
 	if (!$connect) {echo ("MySQL ERROR: " . mysqli_connect_error());}
 	else {echo "SUCCESS: Connection Established to DB\n";}
+	
 	if ($query = mysqli_prepare($connect, "SELECT pkgName, version FROM packages")) {
 		$query ->execute();
 		while ($r = mysqli_fetch_array ($query, MYSQLI_ASSOC)) {
 			$dbPkgName = $r["pkgName"];
 			$dbPkgVersion = $r["version"];
-			
 			// change validation enum to true if found
 			if ($pkgName == $dbPkgName && $version == $dbPkgVersion) {
-				echo "Name and version combo found! ($pkgName-$version)" . PHP_EOL;
-				if ($query = mysqli_prepare($connect, "UPDATE packages SET validated = 'T' WHERE pkgName = $namePkg AND version = $version")) {
+				echo "Name and version combo found! ($pkgName-$version)";
+				if ($query = mysqli_prepare($connect, "UPDATE packages SET validated = '1' WHERE pkgName = $pkgName AND version = $version")) {
 					$query ->execute();
 					return "Package has been validated successfully!" . PHP_EOL;
 				}
@@ -106,15 +109,25 @@ function requestProcessor($request) {
 	if(!isset($request['type'])) {
 		return "ERROR: Unsupported message type!";
 	}
-	
+
 	switch ($request['type']) {
 	case "deployPkg":
-		return doDeployPkg($request['version'], $request['user'], $request['ip'], $request['namepkg'], $request['description'], $request['path']);
-/*	case 'getPkg':
-		return doGetPkg($request['request']);
+		doDeployPkg($request['version'], $request['user'], $request['ip'], $request['namepkg'], $request['description'], $request['path'], $request['tierType']);
+		break;
+
+	case 'getPkg':
+		doGetPkg($request['request']);
+		break;
+
 	case 'rollbackPkg':
-		return doRollback($request['namepkg'], $request['version']);
-}*/
+		doRollback($request['namepkg'], $request['version']);
+		break;
+
+	case 'validatePkg':
+		doValidatePkg($request['namepkg'], $request['version']);
+		break;
+
+}
 	return array("returnCode" => '0', 'message'=>"Server received request and processed.");
 	}
 }
