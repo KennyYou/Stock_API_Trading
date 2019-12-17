f843f8b2c808b5568eefb6a0d257bd95c0eef211
JAWinner <Scorchinq@gmail.com>
2019-12-17 18:11:45 -0500

    pkg_client
    Help, I wanna finish this today

---
+++ deployment/Package_Client.php
@@ -0,0 +1,73 @@
+#!/usr/bin/php
+<?php 
+require_once("../path.inc");
+require_once("../rabbitMQLib.inc");
+require_once("../get_host_info.inc");
+
+exec('./packaging.sh');
+
+$db = new mysqli();
+
+if ($mydb->errno !=0){
+	echo "Connection to Database FAILED: ".$db->error.PHP_EOL;
+	exit(0);
+}
+
+$type = read_stdin("Enter What Type: ");
+$package = read_stdin ("Enter Package: ");
+$tier = 	read_stdin("Where is the package going?: ");
+$name = read_stdin("Package Name: ");
+#rollback
+if ($type == 'rollback' || $type == 'Rollback'){
+	$RBVer = read_stdin("What version to Rollback to: ");
+	
+}
+$version_value = "1";
+
+$query = mysqli_query($db , "SELECT * FROM packages WHERE namePkg = '$name'");
+$count = mysqli_num_rows($querry); 
+
+#package
+if($type == "package"){
+$dex_check = mysqli_query($db, "SELCECT * FROM packages WHERE pkgName = '$name' ORDER BY (pkgVer+0) DESC LIMIT 1");
+$row = mysqli_fetch_assoc($check);
+$ver = $row['pkgVer'];
+echo "Birthing new File...Creating next clone(version) #" . ($version+ $increment_value);
+}
+else{
+	echo "Could not find lost child....replacing child (creating new filename)"; 
+	$version = "0";
+}
+
+#deploy
+if(type == 'deploy'){
+	$dex_check = mysqli_query($db, "SELCECT * FROM packages WHERE pkgName = '$name' ORDER BY (pkgVer+0) DESC LIMIT 1");
+	$row = mysqli_fetch_assoc($check);
+	$ver = $row['pkgVer'];
+	echo "FIRING " . $pkgName . "-" . $version . "NOW";
+}
+
+$client = new rabbitMQLib("deploy.ini" , "testServer");
+request = array();
+$request['type'] = $type;
+$request['package'] = $package;
+$request['tier'] = $tier;
+$request['pkgName'] = $name;
+switch($type){
+	case "bundle":
+		$request['version'] = $ver + $version_value;
+	case "deploy":
+		$request['version'] = $ver;
+	case "rollback":
+		$request['RBVer'] = $RBVer;
+}
+$response = $client->send_request($request);
+
+echo "\n";
+
+if ($type == 'bundle'){
+	rename("/home/jdm68/package/ ","/home/jdm68/package/".$request['namePkg']."-"$request['ver'].".tgz");
+	
+	exec('./packaging.sh');
+}
+?>
