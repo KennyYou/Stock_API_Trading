e26509198a8c454a6260bd4617cd082a93057d24
HemanthSesha <55961217+HemanthSesha@users.noreply.github.com>
2019-12-18 23:27:27 -0500

    FINAL FIX
    I HOPE THIS WORKS

--- deployment/deploymentClient.php
+++ deployment/deploymentClient.php
@@ -1,28 +1,31 @@
 <?php
-require_once("/home/jdm68/git/The_Project/path.inc");
-require_once("/home/jdm68/git/The_Project/rabbitMQLib.inc");
-require_once("/home/jdm68/git/The_Project/get_host_info.inc");
+require_once("../path.inc");
+require_once("../rabbitMQLib.inc");
+require_once("../get_host_info.inc");
 
 echo "Enter information to the deployment server:\n";
 $type = readline("Enter your desired action (bundle, deploy, rollback): ");
 if ($type != "bundle" || $type != "deploy" || $type != "rollback") {
-  echo "Not a valid action! Please try again.";
+  
+}
+else{echo "Not a valid action! Please try again.";
   return;
 }
-
 $pkgMachineType = readline("Enter your origin machine type (FE, BE, DMZ): ");
-if ($type != "FE" || $type != "BE" || $type != "DMZ") {
-  echo "Not a valid origin machine type! Please try again.";
+if ($pkgMachineType != "FE" || $pkgMachineType != "BE" || $pkgMachineType != "DMZ") {
+  
+}
+else{echo "Not a valid action! Please try again.";
   return;
 }
-
 if ($type == "bundle") {$destTier = readline("Enter the tier where the package is coming from (DEV, QA, PROD): ");}
 else {$destTier = readline("Enter the destination tier (DEV, QA, PROD): ");}
-if ($type != "DEV" || $type != "QA" || $type != "PROD") {
-  echo "Not a valid tier type! Please try again.";
+if ($destTier != "DEV" || $destTier != "QA" || $destTier != "PROD") {
+  
+}
+else{echo "Not a valid action! Please try again.";
   return;
 }
-
 $pkgName = readline("Enter the package's name: ");
 if ($type == "rollback") {
   $version = readline("Enter the version number of the problematic package: ");
@@ -36,7 +39,7 @@ else {
 //if (isset($argv[1])) {$msg = $argv[1];}
 //else {$msg = "deploy";}
 
-$client = new rabbitMQClient("/home/jdm68/git/The_Project/deployment/deploy.ini", "testServer");
+$client = new rabbitMQClient("deploy.ini", "testServer");
 $request = array();
 $request['type'] = strtolower($type);
 $request['pkgMachineType'] = strtoupper($pkgMachineType);
@@ -47,5 +50,6 @@ $request['rbVersion'] = $rbVersion;
 $request['userName'] = get_current_user(); // get the local user's userName
 //$request['message'] = $msg;
 $response = $client -> send_request($request);
-echo "Sent your request message for $type!";
+echo "Sent your request message for $type!/n";
+?>
 ?>
