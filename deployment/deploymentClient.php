1c9acf2996514c4098acef47b16d8ecfdf0d3fca
Joseph Mauro <49416852+too-mauro@users.noreply.github.com>
2019-12-18 22:03:38 -0500

    updated again
    yee haw part 3

--- deployment/deploymentClient.php
+++ deployment/deploymentClient.php
@@ -5,26 +5,26 @@ require_once("/home/jdm68/git/The_Project/get_host_info.inc");
 
 echo "Enter information to the deployment server:\n";
 $type = readline("Enter your desired action (bundle, deploy, rollback): ");
-if (strcasecmp($type, "bundle") != 0 || strcasecmp($type, "deploy") != 0 || strcasecmp($type, "rollback") != 0) {
+if ($type != "bundle" || $type != "deploy" || $type != "rollback") {
   echo "Not a valid action! Please try again.";
   return;
 }
 
 $pkgMachineType = readline("Enter your origin machine type (FE, BE, DMZ): ");
-if (strcasecmp($type, "FE") != 0 || strcasecmp($type, "BE") != 0 || strcasecmp($type, "DMZ") != 0) {
+if ($type != "FE" || $type != "BE" || $type != "DMZ") {
   echo "Not a valid origin machine type! Please try again.";
   return;
 }
 
-if ($type == strtolower("bundle")) {$destTier = readline("Enter the tier where the package is coming from (DEV, QA, PROD): ");}
+if ($type == "bundle") {$destTier = readline("Enter the tier where the package is coming from (DEV, QA, PROD): ");}
 else {$destTier = readline("Enter the destination tier (DEV, QA, PROD): ");}
-if (strcasecmp($type, "DEV") != 0 || strcasecmp($type, "QA") != 0 || strcasecmp($type, "PROD") != 0) {
+if ($type != "DEV" || $type != "QA" || $type != "PROD") {
   echo "Not a valid tier type! Please try again.";
   return;
 }
 
 $pkgName = readline("Enter the package's name: ");
-if (strcasecmp($type, "rollback") == 0) {
+if ($type == "rollback") {
   $version = readline("Enter the version number of the problematic package: ");
   $rbVersion = readline("Enter the version you would like to revert to: ");
 }
