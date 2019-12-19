c28ffef44d1f62487e52672e46d40c98ed83d288
Joseph Mauro <49416852+too-mauro@users.noreply.github.com>
2019-12-18 22:48:16 -0500

    Update deployment.sql

--- deployment.sql
+++ deployment.sql
@@ -16,52 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `deploy`
---
-
-DROP TABLE IF EXISTS `deploy`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `deploy` (
-  `bundle_name` varchar(255) DEFAULT NULL,
-  `bundle_version` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `deploy`
---
-
-LOCK TABLES `deploy` WRITE;
-/*!40000 ALTER TABLE `deploy` DISABLE KEYS */;
-/*!40000 ALTER TABLE `deploy` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `install`
---
-
-DROP TABLE IF EXISTS `install`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `install` (
-  `package` varchar(100) NOT NULL,
-  `targetUser` varchar(50) DEFAULT NULL,
-  `version` float NOT NULL,
-  PRIMARY KEY (`package`,`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `install`
---
-
-LOCK TABLES `install` WRITE;
-/*!40000 ALTER TABLE `install` DISABLE KEYS */;
-/*!40000 ALTER TABLE `install` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `packages`
 --
 
@@ -69,13 +23,12 @@ DROP TABLE IF EXISTS `packages`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `packages` (
-  `pkgName` varchar(100) NOT NULL,
-  `version` float NOT NULL,
+  `pkgName` varchar(50) DEFAULT NULL,
+  `version` varchar(10) DEFAULT NULL,
   `uploadDateTime` datetime DEFAULT NULL,
-  `originMachine` varchar(20) DEFAULT NULL,
-  `destTier` varchar(20) DEFAULT NULL,
-  `valid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`pkgName`,`version`)
+  `originMachine` varchar(5) DEFAULT NULL,
+  `destTier` varchar(5) DEFAULT NULL,
+  `valid` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -97,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-18  0:13:32
+-- Dump completed on 2019-12-18 22:46:39
